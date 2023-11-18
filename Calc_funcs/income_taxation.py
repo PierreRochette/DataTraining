c006d998fc8e71defcd0e7aaae2e2af9bc136aba
Pierre Rochette <pierre.rochette@epitech.eu>
2023-11-18 15:14:18 +0100

    update

--- Calc_funcs/income_taxation.py
+++ Calc_funcs/income_taxation.py
@@ -1,4 +1,4 @@
-def children_quotient(element) : 
+def children_shares(element) : 
 
     if element == 0 : 
         number = 0
@@ -8,3 +8,35 @@ def children_quotient(element) :
         number = element - 1 
         
     return number
+
+def mariage_shares(element) : 
+    if element == False : 
+        number = 1
+    else : 
+        number = 2
+
+    return number
+
+def total_shares(a, b) : 
+    
+    return a + b
+
+def income_taxation(quotient, income) : 
+
+    taxable_income = income / quotient
+
+    if taxable_income <= 10777 : 
+        result = 0
+    elif 10778 <= taxable_income <= 27478 : 
+        result = (27478 - taxable_income) * 0.11
+    elif 27479 <= taxable_income <= 78750 : 
+        result = (78750 - taxable_income) * 0.3 + 1837
+    elif 78751 <= taxable_income <= 168994 : 
+        result = (168994 - taxable_income) * 0.41 + 17164.3
+    elif taxable_income > 168994 : 
+        result = (taxable_income - 168995) * 0.45 + 54237.7
+
+    result *= quotient
+
+    return int(result)
+
--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import Calc_funcs.income_taxation as income_taxation
 with open("clients.json", "r") as file : 
     data = json.load(file)
 
+
+
 personnal_income_taxation_data = []
 
 i = 0 
@@ -18,16 +20,10 @@ while i < len(data) - 1 :
         family_quotient, 
         data[i]["annual_income"]
     )
-    
-    tax_data = {
-        "name" : data[i]["name"], 
-        "gender" : data[i]["gender"], 
-        "annual_income" : data[i]["annual_income"], 
-        "income_taxatiion" : income_tax
-    }
 
-    personnal_income_taxation_data.append(tax_data)
+    
 
     i += 1 
 
+print(personnal_income_taxation_data)
 
